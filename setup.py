429a455bafe1d4f3c3f990306ddc4cfd8fc780f7
Noel Maersk <veox+gits@veox.pw>
2021-03-25 17:10:56 +0200

    setup: add Python version requirement, remove Python 2 tag.
    Closes #1 as implemented.
    
    Python 2 support has not actually been tested, and I don't intend
    to, so remove the tag to avoid confusing others.
    
    Not sure if this works with Python 3.3, too, but this'll do as a
    starting point. Tests run with Python 3.5 on gitlab, it seems.

--- setup.py
+++ setup.py
@@ -13,17 +13,17 @@ setup(
     author='Noel Maersk',
     author_email='veox+packages+spamremove@veox.pw',
     url='https://gitlab.com/veox/pygments-lexer-solidity',
-    packages=['pygments_lexer_solidity'],
     classifiers=[
         'Environment :: Plugins',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
     ],
-    keywords='pygments lexer syntax highlight solidity sol yul ethereum',
+    packages=['pygments_lexer_solidity'],
+    python_requires='>=3.3, <4',
     install_requires=[
         'pygments>=2.1'
     ],
+    keywords='pygments lexer syntax highlight solidity sol yul ethereum',
     entry_points="""
     [pygments.lexers]
     solidity = pygments_lexer_solidity:SolidityLexer
